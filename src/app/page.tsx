5055620f0daa41195e895277503cc9f3e0faf735
jiasheng <jiashengguo@outlook.com>
2025-03-15 08:25:06 +0800

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { type NextPage } from "next";
+import { signOut, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+type AuthUser = { id: string; email?: string | null };
+
+const Welcome = ({ user }: { user: AuthUser }) => {
+  const router = useRouter();
+  async function onSignout() {
+    await signOut({ redirect: false });
+    router.push("/signin");
+  }
+  return (
+    <div className="flex gap-4">
+      <h3 className="text-lg">Welcome back, {user?.email}</h3>
+      <button
+        className="text-gray-300 underline"
+        onClick={() => void onSignout()}
+      >
+        Signout
+      </button>
+    </div>
+  );
+};
+
+const SigninSignup = () => {
+  return (
+    <div className="flex gap-4 text-2xl">
+      <Link href="/signin" className="rounded-lg border px-4 py-2">
+        Signin
+      </Link>
+      <Link href="/signup" className="rounded-lg border px-4 py-2">
+        Signup
+      </Link>
+    </div>
+  );
+};
+
+const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") return <p>Loading ...</p>;
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 text-white">
+        <h1 className="text-5xl font-extrabold">My Awesome Blog</h1>
+
+        {session?.user ? (
+          // welcome & blog posts
+          <div className="flex flex-col">
+            <Welcome user={session.user} />
+            <section className="mt-10"></section>
+          </div>
+        ) : (
+          // if not logged in
+          <SigninSignup />
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
